889d19ea5704301a1a432dc402efd3cf9cb602a4
Thinh Phan <pseudothinhphan@gmail.com>
2022-09-21 18:44:14 -0700

    Create 16x16 grid

---
+++ js/eas.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('#container');
+
+for (let z = 0; z < 16; z++) {
+    const row = document.createElement('div');
+    container.appendChild(row).className = 'row';
+}
+
+const rows = document.getElementsByClassName('row');
+
+for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < 16; j++) {
+        const column = document.createElement('div');
+        rows[i].appendChild(column).className = 'column';
+    }
+}
